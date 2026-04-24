808e94d769f5ef9b41fb1d6064cec219136a0e13
Akash Randhawa <93700117+akashrandhawa00@users.noreply.github.com>
2026-04-24 13:11:52 -0400

    investment calculation logic and results display added

--- src/util/investment.js
+++ src/util/investment.js
@@ -1,9 +1,3 @@
-// This function expects a JS object as an argument
-// The object should contain the following properties
-// - initialInvestment: The initial investment amount
-// - annualInvestment: The amount invested every year
-// - expectedReturn: The expected (annual) rate of return
-// - duration: The investment duration (time frame)
 export function calculateInvestmentResults({
   initialInvestment,
   annualInvestment,
@@ -27,12 +21,9 @@ export function calculateInvestmentResults({
   return annualData;
 }
 
-// The browser-provided Intl API is used to prepare a formatter object
-// This object offers a "format()" method that can be used to format numbers as currency
-// Example Usage: formatter.format(1000) => yields "$1,000"
-export const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
+export const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
   minimumFractionDigits: 0,
   maximumFractionDigits: 0,
 });
